0ea0e877b00d21517702aeb5a1eecc2a7581899b
varunification <letter2varun@gmx.com>
2023-04-15 18:00:05 +0530

    Add project files.

---
+++ SwankiNails2/Controllers/ServicesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwankiNails2.Controllers
+{
+    public class ServicesController: Controller
+    {
+        private readonly ILogger<ServicesController> _logger;
+
+        public ServicesController(ILogger<ServicesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+    }
+}
