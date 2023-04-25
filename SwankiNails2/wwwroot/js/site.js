0307f687e68eb04c04ca0c03af6f704441c42702
varunification <letter2varun@gmx.com>
2023-04-25 23:53:31 +0530

    some bug fixes

--- SwankiNails2/wwwroot/js/site.js
+++ SwankiNails2/wwwroot/js/site.js
@@ -1,14 +1,14 @@
-﻿
+﻿if (window.location.href.indexOf('/Bookings/') >-1) {
     // Email validation
     const emailInput = document.getElementById('email');
     const emailError = document.getElementById('email-error');
 
-    emailInput.addEventListener('input', function() {
-      if (!emailInput.checkValidity()) {
-        emailError.textContent = 'Please enter a valid email address.';
-      } else {
-        emailError.textContent = '';
-      }
+    emailInput.addEventListener('input', function () {
+        if (!emailInput.checkValidity()) {
+            emailError.textContent = 'Please enter a valid email address.';
+        } else {
+            emailError.textContent = '';
+        }
     });
 
     // Phone number validation
@@ -16,11 +16,11 @@
     const mobileError = document.getElementById('mobile-error');
     const phoneRegex = /^\d{10}$/;
 
-    mobileInput.addEventListener('input', function() {
-      if (!phoneRegex.test(mobileInput.value)) {
-        mobileError.textContent = 'Please enter a 10-digit phone number.';
-      } else {
-        mobileError.textContent = '';
-      }
+    mobileInput.addEventListener('input', function () {
+        if (!phoneRegex.test(mobileInput.value)) {
+            mobileError.textContent = 'Please enter a 10-digit phone number.';
+        } else {
+            mobileError.textContent = '';
+        }
     });
-
+}
